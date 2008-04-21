7eaf3871a3b4faf422b38dcafdc4a19d2183414d
lhughes33472 <lhughes33472@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-21 15:04:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/metamagic/trunk@42005 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ metamagic.php
@@ -0,0 +1,158 @@
+<?php
+/*
+Plugin Name: MetaMagic 
+Plugin URI: http://blog.hughestech.com/blog/metamagic/
+Description: This WordPress Plugin Generates meta description tags and meta keywords tag for your single posts automatically.
+Version: 1.0
+Author: HughesTech Labs
+Author URI: http://blog.hughestech.com/blog/
+
+Copyrighted (C)2008 Hughes Technologies, Inc. email : plugins@hughestech.com
+
+Hughes Technologies, Inc. grants a license of use for MetaMagic.
+It is our hope that it will be useful, but users of MetaMagic use it
+without any warranty; without even the implied warranty of merchantability
+or fitness for a particular application or purpose. When using MetaMagic,
+users use at their own risk! Hughes Technologies, Inc. will not be
+responsible for, or held libel for anything pertaining to the use of
+MetaMagic! Anyone can use MetaMagic, but modifying, reverse engineering,
+selling, Sub-licensing, licensing, bundling with other software for sale,
+or for free, or given away as a promotion is strictly forbidden, without
+the written permission from Hughes Technologies, Inc. Hughes also reserves
+the right to revoke any license at anytime for any reason with or without
+cause, or providing a reason for said license revoke! By using MetaMagic
+by Hughes Technologies, Inc., you agree to all the above terms!
+*/
+
+
+if ( !get_option('metamagic_options') ) {
+
+$options = array(
+		'metamagic_enable'		=> 1,
+		'metamagic_description'	=> 1,
+		'metamagic_keywords'	=> 1,
+		);
+					
+update_option( 'metamagic_options', $options );
+
+}
+
+function metamagic_main() {
+
+ $options = get_option( 'metamagic_options' );
+
+      if( $options['metamagic_enable'] == 1 ) {
+           if( is_single() ) {  // Make sure for single post, or all posts will have the same meta tags
+               global $post; 
+               $description = '';
+               $keywords = '';
+               echo "\n";
+               echo '<!-- MetaMagic WordPress plugin; http://blog.hughestech.com/blog/metamagic/ -->' . "\n";
+	       if( $options['metamagic_description'] == 1 ) {
+		   $recentpost = get_post($post->ID); 
+		   $content = $recentpost->post_content; 
+                   $acontent = strip_tags($content,'<u></u>');
+                   $start = strpos($acontent, '<u>');
+                   if($start) {
+                       $end = strpos($acontent, '</u>');
+                       $content = substr($acontent, $start+3, $end - $start -1);
+                       $acontent = strip_tags($content);
+                       $content = str_replace("\r", ' ', $acontent);
+                       $content = str_replace("\n", ' ', $content);
+                       $content = str_replace("\t", '', $content);  
+                       $description = $content;
+                       echo '<meta name="description" content="'. $description .'" />' . "\n";
+                      }
+                    }
+                   if( $options['metamagic_keywords'] == 1 ) {                
+                       if ( function_exists("get_the_tags") ) {
+                                $wordpress23x = true;  
+				$tags = get_the_tags( $post->ID );
+			   }
+			   else if( function_exists("UTW_ShowTagsForCurrentPost") ) {
+				global $utw;
+				$tags = $utw->GetTagsForPost( $post->ID, 8);		
+			   }
+                             
+                             if( $tags ) {
+				 $cnt = 0;
+				 foreach( $tags as $tboy ) {
+                                    if($n < 20 ) {
+					if( $wordpress23x ) {
+						$keywords .= $tboy->name . ', ';
+						}
+						else {
+						      $keywords .=  $tboy->tag . ', '; 
+						     }
+						     $cnt++;
+						            }
+						else {
+							break;
+						}
+					}
+				}
+                                $tempkey = substr_replace($keywords,"",-2);
+                                $keywords = $tempkey;
+                                echo '<meta name="keywords" content="' . $keywords .'" />' . "\n";	
+	             }   
+                	
+            }
+        }
+}
+
+
+function metamagic_menu() {
+	
+	add_options_page('MetaMagic', 'MetaMagic', 8, basename(__FILE__), 'metamagic_admin');
+	
+}
+
+function metamagic_admin() {
+
+	if ( isset($_POST['metamagic_submit'] ) ) {
+		metamagic_update_options();
+	}
+	
+	$options = get_option( 'metamagic_options' );
+	
+	$show_enable      =	( $options['metamagic_enable']       == 1 ) 	?	'checked="checked"' : '';
+	$show_description =	( $options['metamagic_description']  == 1 ) 	?	'checked="checked"' : '';
+	$show_keywords     =	( $options['metamagic_keywords']     == 1 )	?	'checked="checked"' : '';
+		
+	echo '<div class="wrap">';
+	echo '<h2>' . __('MetaMagic Options') . '</h2>';
+	echo '<form method="post" action="">';
+	echo '<fieldset>';
+	echo '<h3>Select Automatic Features:</h3>';
+	echo '<p><input type="checkbox" id="metamagic_enable" name="metamagic_enable" '. $show_enable.' /> <label for="MetaMagic_enable">'. __("Enable ( If enabled, meta tags will be added to your header )") .'</label></p>';
+	echo '<p><input type="checkbox" id="metamagic_description" name="metamagic_description" '. $show_description.' /> <label for="MetaMagic_description">'. __("Meta Description ( If checked, meta description will be your underlined text within your post)") .'</label></p>';
+	echo '<p><input type="checkbox" id="metamagic_keywords" name="metamagic_keywords" '. $show_keywords.' /> <label for="MetaMagic_keywords">'. __("Meta Keywords ( if checked, your tags will be added as meta keywords when posting )") .'</label></p>';
+	echo '<p class="submit">';
+	echo '<input type="submit" name="metamagic_submit" value="Save settings" />';
+	echo '</fieldset>';
+	echo '</form>';
+	echo '</div>';
+
+}
+
+function metamagic_update_options() {
+
+	$options = array();
+	$options['metamagic_enable']      =	( $_POST['metamagic_enable']       == "on" )	?	1 : 0;
+	$options['metamagic_description'] =	( $_POST['metamagic_description']  == "on" )	?	1 : 0;
+	$options['metamagic_keywords']    =	( $_POST['metamagic_keywords']     == "on" )	?	1 : 0;
+	        	
+	update_option('metamagic_options', $options);
+	
+	echo '<div class="updated fade"><p><strong>'. __('Options saved.') .'</strong></p></div>';
+		
+      }
+
+
+add_action('admin_menu','metamagic_menu');
+
+
+add_action('wp_head','metamagic_main', 1);
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== MetaMagic ===
+Donate link: http://blog.hughestech.com/blog/metamagic/
+Tags: meta keywords, meta-description, meta-tags, automatic, tags, keywords, seo
+Stable tag: trunk
+Tested up to: 2.5
+
+This creates meta keywords and meta descriptions for single posts.
+
+== Description ==
+
+This WordPress plugin creates the meta keywords tag and the meta descriptions tag for single posts automatically, simply underline a phrase in your post while in the WordPress Editor and that will be the meta description tag content. The meta keywords content is generated from the tags in your post. In the Options Panel you can toggle meta keywords tag only, or meta description tags only, or both, or none.
+
+== Installation ==
+
+1. Download the plugin.
+1. Upload the plugin to your wp-content/plugins directory.
+1. Activate the plugin.
+1. Configure it through the Options panel.
+
+That's all folks, have fun and improve your ranking....
