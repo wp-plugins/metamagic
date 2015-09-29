3ad586df10782ed9906da9e37e556266f8d0a84a
lhughes33472 <lhughes33472@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-29 16:24:09 +0000

    Now supports meta descriptions for pages
    git-svn-id: https://plugins.svn.wordpress.org/metamagic/trunk@1255954 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- metamagic.php
+++ metamagic.php
@@ -3,11 +3,11 @@
 Plugin Name: MetaMagic SEO Plugin 
 Plugin URI: http://blog.hughestech.com/blog/metamagic/
 Description: This SEO WordPress Plugin Generates meta description tags and meta keywords tag for your blog posts automatically.
-Version: 1.3
+Version: 1.4
 Author: HughesTech Labs
 Author URI: http://blog.hughestech.com/blog/
 
-Copyrighted (C)2008 - 2014 Hughes Technologies, Inc. email : plugins@hughestech.com
+Copyrighted (C)2008 - 2015 Hughes Technologies, Inc. email : plugins@hughestech.com
 
 Hughes Technologies, Inc. grants a license of use for MetaMagic.
 It is our hope that it will be useful, but users of MetaMagic use it
@@ -28,7 +28,7 @@ by Hughes Technologies, Inc., you agree to all the above terms!
 if ( !get_option('metamagic_options') ) {
 
 $options = array(
-		'metamagic_enable'		=> 1,
+		'metamagic_enable'	=> 1,
 		'metamagic_description'	=> 1,
 		'metamagic_keywords'	=> 1,
 		);
@@ -37,7 +37,8 @@ update_option( 'metamagic_options', $options );
 
 }
 
-function metamagic_main() {
+function metamagic_main()
+{
 
  $options = get_option( 'metamagic_options' );
 
@@ -51,48 +52,20 @@ function metamagic_main() {
 	       if( $options['metamagic_description'] == 1 ) {
 		   $recentpost = get_post($post->ID); 
 		   $content = $recentpost->post_content;
-		   //Legacy support for version 1.0
-                   $acontent = strip_tags($content,'<u></u>');
-                   
-                   if(preg_match('/<u>/',$acontent)){
-                       $legacy =1;
-                     }else{
-                       $legacy =0;
-                     }        
-                   if($legacy == 0){
-                      $acontent = strip_tags($content,'<MetaMagic></MetaMagic>');
-                     }  
-                   if($legacy==1){
-                      $start = strpos($acontent, '<u>');
-                     }else{
-                      $start = strpos($acontent, '<MetaMagic>');
-                     }
-                      
-    //legacy support for underline      
-    if(($start) && ($legacy==1))
-      {
-        $end = strpos($acontent, '</u>');
-        $content = substr($acontent, $start+3, $end - $start -1);
-        $acontent = strip_tags($content);
-        $content = str_replace("\r", ' ', $acontent);
-        $content = str_replace("\n", ' ', $content);
-        $content = str_replace("\t", '', $content);  
-        $description = $content;
-        echo '<meta name="description" content="'. $description .'" />' . "\n";
-      }
-    if(($start) && ($legacy==0))
-      {
-        $end = strpos($acontent, '</MetaMagic>');
-        $content = substr($acontent, $start+11, $end - $start -1);
-        $acontent = strip_tags($content);
-        $content = str_replace("\r", ' ', $acontent);
-        $content = str_replace("\n", ' ', $content);
-        $content = str_replace("\t", '', $content);
-        $description = $content;
-        echo '<meta name="description" content="'. $description .'" />' . "\n";
-      }               
-  }
-    if( $options['metamagic_keywords'] == 1 ) {                
+                   $acontent = strip_tags($content,'<MetaMagic></MetaMagic>');
+                   $start = strpos($acontent, '<MetaMagic>');                                                  
+                   $end = strpos($acontent, '</MetaMagic>');
+                   $content = substr($acontent, $start+11, $end - $start -1);
+                   $acontent = strip_tags($content);
+                   $content = str_replace("\r", ' ', $acontent);
+                   $content = str_replace("\n", ' ', $content);
+                   $content = str_replace("\t", '', $content);
+                   $description = $content;
+                   echo '<meta name="description" content="'. $description .'" />' . "\n";
+                 }
+
+    if( $options['metamagic_keywords'] == 1 ) 
+    {                
       if ( function_exists("get_the_tags"))
         {
           $wordpress23x = true;  
@@ -106,7 +79,7 @@ function metamagic_main() {
                              if( $tags ) {
 				 $cnt = 0;
 				 foreach( $tags as $tboy ) {
-                                    if($n < 20 ) {
+                                    if($cnt < 20 ) {
 					if( $wordpress23x ) {
 						$keywords .= $tboy->name . ', ';
 						}
@@ -125,9 +98,34 @@ function metamagic_main() {
                                 echo '<meta name="keywords" content="' . $keywords .'" />' . "\n";	
 	             }   
                 	
-            }
-        }
-}
+    }
+   } //is single post end
+        
+  if( is_page() ) {  //Single page being displayed New for Version 1.4             
+    global $page; 
+    $description = '';
+    $keywords = '';
+    echo "\n";
+    echo '<!-- MetaMagic WordPress plugin; http://blog.hughestech.com/blog/metamagic/ -->' . "\n";
+    if( $options['metamagic_description'] == 1 )
+     {
+       $recentpost =  get_page($page->ID); 
+       $content = $recentpost->post_content;
+       $acontent = strip_tags($content,'<MetaMagic></MetaMagic>');
+       $start = strpos($acontent, '<MetaMagic>');                                                  
+       $end = strpos($acontent, '</MetaMagic>');
+       $content = substr($acontent, $start+11, $end - $start -1);
+       $acontent = strip_tags($content);
+       $content = str_replace("\r", ' ', $acontent);
+       $content = str_replace("\n", ' ', $content);
+       $content = str_replace("\t", '', $content);
+       $description = $content;
+       echo '<meta name="description" content="'. $description .'" />' . "\n";
+     }
+              	
+ } //is page end
+
+} //main function end
 
 add_action('admin_menu','metamagic_menu');
 
@@ -155,8 +153,8 @@ function metamagic_admin() {
 	echo '<fieldset>';
 	echo '<h3>Select Automatic Features:</h3>';
 	echo '<p><input type="checkbox" id="metamagic_enable" name="metamagic_enable" '. $show_enable.' /> <label for="MetaMagic_enable">'. __("Enable ( If enabled, meta tags will be added to your header )") .'</label></p>';
-	echo '<p><input type="checkbox" id="metamagic_description" name="metamagic_description" '. $show_description.' /> <label for="MetaMagic_description">'. __("Meta Description ( If checked, the meta description will be any text within these tags: <b>&ltMetaMagic&gttext in your post&lt/MetaMagic&gt)</b>") .'</label></p>';
-	echo '<p><input type="checkbox" id="metamagic_keywords" name="metamagic_keywords" '. $show_keywords.' /> <label for="MetaMagic_keywords">'. __("Meta Keywords ( if checked, your tags will be added as meta keywords when posting )") .'</label></p>';
+	echo '<p><input type="checkbox" id="metamagic_description" name="metamagic_description" '. $show_description.' /> <label for="MetaMagic_description">'. __("Meta Description ( If checked, the meta description will be any text within these tags: <b>&ltMetaMagic&gttext in your Post(s) or Page(s)&lt/MetaMagic&gt)</b>") .'</label></p>';
+	echo '<p><input type="checkbox" id="metamagic_keywords" name="metamagic_keywords" '. $show_keywords.' /> <label for="MetaMagic_keywords">'. __("Meta Keywords ( if checked, your tags will be added as meta keywords when Posting Only )") .'</label></p>';
 	echo '<p class="submit">';
 	echo '<input type="submit" name="metamagic_submit" value="Save settings" />';
 	echo '</fieldset>';
@@ -164,13 +162,16 @@ function metamagic_admin() {
       echo 'If you enjoy using MetaMagic, please give it 5 stars on WordPress<br />'; 
       echo '<a href="http://wordpress.org/support/view/plugin-reviews/metamagic" target="_blank" title="MetaMagic Reviews">Rate MetaMagic</a><br /><br />';
       echo 'Please Help our Dev Team:<br />';
-      echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">';
+      echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">';
       echo '<input name="cmd" type="hidden" value="_s-xclick" /><br />';
       echo '<input name="hosted_button_id" type="hidden" value="YABX66T8E52ZN" /><br />';
       echo '<input alt="PayPal - The safer, easier way to pay online!" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" type="image" /><br />';
       echo '<img src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" alt="" width="1" height="1" border="0" /></form><br /><br />';
+      echo '<strong>Important Note:</strong><br />';
+      echo 'Only Add the <b>&ltMetaMagic&gt</b> tags wrapped around some phrase<b>&lt/MetaMagic&gt</b><br />';
+      echo 'while editing within the <b>Text Editor Tab, Not</b> the <b>Visual Editor Tab</b>!<br /><br />';
       echo 'That\'s all folks, have fun and improve your SE Rankings....';
-	echo '</div>';
+      echo '</div>';
 
 }
 
